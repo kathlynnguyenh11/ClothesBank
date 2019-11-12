a421c342320f2a334630ba86da3203e6f0fbf6ed
kathlyn nguyen <tn224@njit.edu>
2019-11-11 21:07:17 -0500

    'login'

--- initDB.php
+++ initDB.php
@@ -1,61 +1,72 @@
 <?php
-#turn error reporting on
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require('config.php');
-echo "Loaded Host: " . $host;
-echo "for testing, username: hang, password: 12";
-$response="";
-$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
-try{
-    $db = new PDO($conn_string, $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
-    if (isset($_POST["username"]) && isset($_POST["password"])){
-        $username = $_POST["username"];
-            $password = $_POST["password"];
-    
-        $query = "select * from project1 where username='$username' AND password='$password'";
-        echo "<br>Connected";
-        $stmt = $db->prepare($query);
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $stmt->execute(array(":username"=> $username,":password"=>$password));      
-        $results = $stmt->fetch(PDO::FETCH_ASSOC);
-        if(($results['username'] == $username) && ($results['password'] == $password)){
-        $response = "<br>Successfully logged in";}
-        else {
-                if(!$stmt->errorInfo()) {
-                    print_r($stmt->errorInfo());}
-                    else {
-                        $response = "<br><br>The username OR password is incorrect or does not exist";}
-        }
-    }
-    else { 
-            $response = "The username or password field is empty";
-    }
-    echo $response;
-    
-}
-    catch(Exception $e){
-        echo $e->getMessage();
-        exit("Something went wrong");
-    }
-
+    session.start();
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
 ?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Register</title>
-    </head>
+    <head></head>
     <body>
-        <h1>Register</h1>
-        <form action="initDB.php" method="post">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username"><br>
-            <label for="password">Password</label>
-            <input type="text" id="password" name="password"><br>
-            <input type="submit" name="register" value="Register"></button>
+        <form method="POST"/>
+            <input type="text" name="username"/>
+            <input type="password" name="password"/>
+            <input type="submit" value="Login"/>
         </form>
     </body>
 </html>
+
+<?php>
+
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        $user = $_POST["username"];
+        $pass = $_POST["password"];
+        try{
+            require("config.php");
+            $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+            $db = new PDO($conn_string,$username,$password);
+            $stmt = $db->prepare("select id, username, password from `Users` where username = :username LIMIT 1");
+			$stmt->execute(array(":username"=>$user));
+            //print_r($stmt->errorInfo());
+            $results = $stmt->fetch(PDO::FETCH_ASSOC);
+			//echo var_export($results, true);
+			if($results && count($results) > 0){
+				//$hash = password_hash($pass, PASSWORD_BCRYPT);
+				if(password_verify($pass, $results['password'])){
+                    echo "Welcome, " . $results["username"];
+					echo "[" . $results["id"] . "]";
+					$user = array("id"=> $results['id'],
+								"name"=> $results['username']
+								);
+					$_SESSION['user'] = $user;
+					echo var_export($user, true);
+					echo var_export($_SESSION, true);
+					header("Location: samplelandingpage.php");
+					
+				}
+				else{
+					echo "Invalid password";
+                }
+                echo "Welcome, " . $results["username"];
+					echo "[" . $results["id"] . "]";
+					$user = array("id"=> $results['id'],
+								"name"=> $results['username']
+								);
+					$_SESSION['user'] = $user;
+					echo var_export($user, true);
+					echo var_export($_SESSION, true);
+					header("Location: samplelandingpage.php");
+					
+				}
+				else{
+					echo "Invalid password";
+                }
+            
+			    else{
+					echo "Invalid username";
+			    }
+            }
+            catch(Exception $e){
+                echo $e->getMessage();
+            }
+    }
+?>
