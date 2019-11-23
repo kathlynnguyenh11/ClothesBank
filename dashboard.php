3e337acdb2fabb6d6bf12a4cfacaeb515a7ecc32
kath <kathlyn.hang.nguyen11@gmail.com>
2019-11-23 16:43:12 -0500

    welcome name

--- dashboard.php
+++ dashboard.php
@@ -1,2 +1,24 @@
+
+<html>
+<head>
+<style>
+.nav{padding:1%;}
+</style>
+</head>
+<body>
 <?php
+echo "<strong>Hi " . $_SESSION['firstname'] . ", welcome back!</strong>";
+//include_once("partials/header.php");
+//include_once("helpers/functions.php");
 ?>
+
+<section></section>
+<section>
+<header>Items Due Soon</header>
+<p>Item one</p>
+<p>Item two</p>
+<p>Item three</p>
+<p>...</p>
+</section>
+</body>
+</html>
