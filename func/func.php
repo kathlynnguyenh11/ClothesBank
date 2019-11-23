fcf828b66856077c5bfc8fbc8558fe48768e0637
kath <kathlyn.hang.nguyen11@gmail.com>
2019-11-23 17:08:13 -0500

    session

--- func/func.php
+++ func/func.php
@@ -1,7 +1,7 @@
 <?php
 function get_username(){
-	if(isset($_SESSION['user']['firstname'])){
-		echo $_SESSION['user']['firstname'];
+	if(isset($_SESSION['user']['name'])){
+		echo $_SESSION['user']['name'];
 	}
 	else{
 		echo "[Session missing]";
