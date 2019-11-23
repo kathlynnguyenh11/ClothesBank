37e92051511595092fa6b1c9452edda025774444
kath <kathlyn.hang.nguyen11@gmail.com>
2019-11-23 18:36:47 -0500

    profile

--- profile.php
+++ profile.php
@@ -1,2 +1,35 @@
 <?php
+$user=$_SESSION['user'];
+$result = mysql_query("SELECT * FROM member where username='$user'");
+while($row = mysql_fetch_array($result))
+{ 
+$fname=$row['firstname'];
+$lname=$row['lastname'];
+$contact=$row['contact_number'];
+$email=$row['email'];
+$zip=$row['zip'];
+}
 ?>
+<table width="398" border="0" align="center" cellpadding="0">
+  <tr>
+    <td height="26" colspan="2">Your Profile Information </td>
+	//<td><div align="right"><a href="index.php">logout</a></div></td>
+  </tr>
+  <tr>
+    <td valign="top"><div align="left">LastName:</div></td>
+    <td valign="top"><?php echo $lname ?></td>
+  </tr>
+  <tr>
+    <td valign="top"><div align="left">Email:</div></td>
+    <td valign="top"><?php echo $email ?></td>
+  </tr>
+  <tr>
+    <td valign="top"><div align="left">Zip Code:</div></td>
+    <td valign="top"><?php echo $zip ?></td>
+  </tr>
+  <tr>
+    <td valign="top"><div align="left">Contact No.: </div></td>
+    <td valign="top"><?php echo $contact ?></td>
+  </tr>
+</table>
+//<p align="center"><a href="index.php"></a></p>
