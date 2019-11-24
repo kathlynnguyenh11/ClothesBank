dea124f867fed1f07088d7ae33e3e301a2300e09
kathlyn nguyen <tn224@njit.edu>
2019-11-24 17:32:36 -0500

    donation table

---
+++ donationTab.php
@@ -0,0 +1,30 @@
+<?php
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    require('config.php');
+    echo "Loaded Host: " . $host;
+    $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+    try{
+	    $db = new PDO($conn_string, $username, $password);
+	    echo "Connected";
+	    $query = "create table if not exists `donation`(
+		        `id` int auto_increment not null,
+		        `username` varchar(30) not null unique,
+                `type` varchar(10) not null unique,
+                `donation` varchar(100) not null,
+                `condition` varchar(30) not null,
+		        PRIMARY KEY (`id`)
+		        ) CHARACTER SET utf8 COLLATE utf8_general_ci";
+	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	    $stmt = $db->prepare($query);
+	    print_r($stmt->errorInfo());
+	    $r = $stmt->execute();
+	    echo "<br>" . ($r>0?"Created table or already exists":"Failed to create table") . "<br>";
+	    unset($r);
+    }
+    catch(Exception $e){
+	    echo $e->getMessage();
+	    exit("Something went wrong");
+    }
+?>
