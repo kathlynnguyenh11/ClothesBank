ec08ef64c0cb5f9e73e41b94eb30c6cbfeaec0aa
kath <kathlyn.hang.nguyen11@gmail.com>
2019-11-23 19:09:24 -0500

    error

--- profile.php
+++ profile.php
@@ -1,13 +1,13 @@
 <?php
-session_start();
+/*session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-
-include_once("func/func.php");
+*/
 $user = $_SESSION['username'];
 echo $user;
+include_once("func/func.php");
+
 /*try{
 	require("config.php");
 	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
